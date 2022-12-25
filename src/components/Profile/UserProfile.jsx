c0bf2b28a35cd43d3579802b8e374310e6fc0889
MinhThu2507 <113906607+MinhThu2507@users.noreply.github.com>
2022-12-25 22:36:46 +0700

    Update UserProfile.jsx

--- src/components/Profile/UserProfile.jsx
+++ src/components/Profile/UserProfile.jsx
@@ -1,5 +1,22 @@
-import { MDBBreadcrumb, MDBBreadcrumbItem, MDBCard, MDBCardBody, MDBCardImage, MDBCardText, MDBCol, MDBContainer, MDBRow } from 'mdb-react-ui-kit';
-import React, { useEffect, useState } from 'react'
+import {
+  MDBBreadcrumb,
+  MDBBreadcrumbItem,
+  MDBCard,
+  MDBCardBody,
+  MDBCardFooter,
+  MDBCardImage,
+  MDBCardText,
+  MDBCol,
+  MDBContainer,
+  MDBRow,
+} from "mdb-react-ui-kit";
+import React, { useEffect, useState } from "react";
+import * as Yup from "yup";
+import { TextField } from "@mui/material";
+import { motion } from 'framer-motion'
+
+import { useFormik } from "formik";
+import { toast } from "react-toastify";
 // Hook
 import { useDispatch, useSelector } from "react-redux";
 
@@ -29,6 +46,42 @@ const UserProfile = () => {
   useEffect(() => {
     disatch(getProfileApi())
   }, [])
+
+  useEffect(() => {
+    setProfile(getProfile);
+  }, [userProfile]);
+
+  const emailLocalStore = getStoreJson(USER_LOGIN)?.email;
+  const formik = useFormik({
+    initialValues: {
+      email: profile?.email || '',
+      password: profile?.password || '',
+      name: profile?.name || '',
+      gender: true,
+      phone: profile?.phone || '',
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().required("Required"),
+      password: Yup.string().required("Required"),
+      name: Yup.string().required("Required"),
+      phone: Yup.string().required("Required"),
+    }),
+    onSubmit: (values) => {
+      console.log('users', values)
+      try {
+        if (values.email === emailLocalStore) {
+          console.log(emailLocalStore, values.email)
+          disatch(updateProfile(values));
+          toast.success("Update profile successfully");
+        } else {
+          toast.fail("Update profile fail");
+        }
+        console.log(values)
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
   return (
     <section style={{ backgroundColor: "#eee", padding: "50px 0 0 0" }}>
       <MDBContainer>
@@ -49,64 +102,175 @@ const UserProfile = () => {
           <MDBCol lg="4">
             <MDBCard className="mb-4 pb-3">
               <MDBCardBody className="text-center">
-                <MDBCardImage
-                  src={arrImage[Math.floor(Math.random() * arrImage.length)]}
-                  alt="avatar"
-                  className="rounded-circle"  
-                  style={{ width: '150px', height:'150px' }}
-                  fluid />
-                <p className="text-muted mb-1 mt-2">Hi, xin chào các bạn mình là {userProfile?.name} </p>
-                <p className="text-muted mb-4">Mình là học viên tại Cybersoft</p>
+                {profile?.image ? (
+                  <MDBCardImage
+                    src={profile?.image}
+                    alt="avatar"
+                    className="rounded-circle"
+                    style={{ width: "150px", height: "150px" }}
+                    fluid
+                  />
+                ) : (
+                  <MDBCardImage
+                    src={arrImage[Math.floor(Math.random() * arrImage.length)]}
+                    alt="avatar"
+                    className="rounded-circle"
+                    style={{ width: "150px", height: "150px" }}
+                    fluid
+                  />
+                )}
+
+                <p className="text-muted mb-1 mt-2">
+                  Hi, xin chào các bạn mình là {profile?.name}{" "}
+                </p>
+                <p className="text-muted mb-4">
+                  Mình là học viên tại Cybersoft
+                </p>
               </MDBCardBody>
             </MDBCard>
           </MDBCol>
           <MDBCol lg="8">
             <MDBCard className="mb-4">
               <MDBCardBody>
-                <MDBRow>
-                  <MDBCol sm="3">
-                    <MDBCardText>Full Name</MDBCardText>
-                  </MDBCol>
-                  <MDBCol sm="9">
-                    <MDBCardText className="text-muted">{userProfile?.name}</MDBCardText>
-                  </MDBCol>
-                </MDBRow>
-                <hr />
-                <MDBRow>
-                  <MDBCol sm="3">
-                    <MDBCardText>Email</MDBCardText>
-                  </MDBCol>
-                  <MDBCol sm="9">
-                    <MDBCardText className="text-muted">{userProfile?.email}</MDBCardText>
-                  </MDBCol>
-                </MDBRow>
-                <hr />
-                <MDBRow>
-                  <MDBCol sm="3">
-                    <MDBCardText>Phone</MDBCardText>
-                  </MDBCol>
-                  <MDBCol sm="9">
-                    <MDBCardText className="text-muted">{userProfile?.phone}</MDBCardText>
-                  </MDBCol>
-                </MDBRow>
-                <hr />
-                <MDBRow>
-                  <MDBCol sm="3">
-                    <MDBCardText>Carrer</MDBCardText>
-                  </MDBCol>
-                  <MDBCol sm="9">
-                    <MDBCardText className="text-muted">Web developer</MDBCardText>
-                  </MDBCol>
-                </MDBRow>
-                <hr />
-                <MDBRow>
-                  <MDBCol sm="3">
-                    <MDBCardText>Address</MDBCardText>
-                  </MDBCol>
-                  <MDBCol sm="9">
-                    <MDBCardText className="text-muted">Ho Chi Minh</MDBCardText>
-                  </MDBCol>
-                </MDBRow>
+                <form action="" onSubmit={formik.handleSubmit}>
+                  <MDBRow style={{ display: 'flex', alignItems: 'center' }}>
+                    <MDBCol sm="3">
+                      <MDBCardText>Email</MDBCardText>
+                    </MDBCol>
+                    <MDBCol sm="9">
+                      <TextField
+                        id="outlined-basic"
+                        value={formik.values.email}
+                        label="Email"
+                        variant="outlined"
+                        className="form-control form-control-lg my-2"
+                        name="email"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                      />
+                      {formik.errors.email ?? (
+                        <p className="text-danger">{formik.errors.email}</p>
+                      )}
+                    </MDBCol>
+                    
+                  </MDBRow>
+                  <hr />
+                  <MDBRow style={{ display: 'flex', alignItems: 'center' }}>
+                    <MDBCol sm="3">
+                      <MDBCardText>Name</MDBCardText>
+                    </MDBCol>
+                    <MDBCol sm="9">
+                      <TextField
+                        id="outlined-basic"
+                        value={formik.values.name}
+                        label="Username"
+                        variant="outlined"
+                        className="form-control form-control-lg my-2"
+                        name="name"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                      />
+                         {formik.errors.name ?? (
+                        <p className="text-danger">{formik.errors.name}</p>
+                      )}
+                    </MDBCol>
+                  </MDBRow>
+                  <hr />
+                  <MDBRow style={{ display: 'flex', alignItems: 'center' }}>
+                    <MDBCol sm="3">
+                      <MDBCardText>Phone</MDBCardText>
+                    </MDBCol>
+                    <MDBCol sm="9">
+                      <TextField
+                        id="outlined-basic"
+                        value={formik.values.phone}
+                        label="Phone Number"
+                        variant="outlined"
+                        className="form-control form-control-lg my-2"
+                        name="phone"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                      />
+                         {formik.errors.phone ?? (
+                        <p className="text-danger">{formik.errors.phone}</p>
+                      )}
+                    </MDBCol>
+                  </MDBRow>
+                  <hr />
+                  <MDBRow style={{ display: 'flex', alignItems: 'center' }}>
+                    <MDBCol sm="3">
+                      <MDBCardText>Password</MDBCardText>
+                    </MDBCol>
+                    <MDBCol sm="9">
+                      <TextField
+                        id="outlined-basic"
+                        value={formik.values.password}
+                        label="Password"
+                        variant="outlined"
+                        className="form-control form-control-lg my-2"
+                        name="password"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                      />{" "}
+                         {formik.errors.password ?? (
+                        <p className="text-danger">{formik.errors.password}</p>
+                      )}
+                    </MDBCol>
+                  </MDBRow>
+                  <hr />
+                  <MDBRow>
+                    <MDBCol sm="3" style={{ display: 'flex', alignItems: 'center' }}>
+                      <MDBCardText>Gender</MDBCardText>
+                    </MDBCol>
+                    <MDBCol sm="9">
+                      <div>
+                        <div className="form-check form-check-inline">
+                          <input
+                            className="form-check-input"
+                            defaultValue="true"
+                            onChange={formik.handleChange}
+                            defaultChecked
+                            type="radio"
+                            name="gender"
+                            id="inlineRadio1"
+                          />
+                          <label
+                            className="form-check-label"
+                            htmlFor="inlineRadio1"
+                          >
+                            Male
+                          </label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                          <input
+                            className="form-check-input"
+                            onChange={formik.handleChange}
+                            type="radio"
+                            defaultValue="false"
+                            name="gender"
+                            id="inlineRadio2"
+                          />
+                          <label
+                            className="form-check-label"
+                            htmlFor="inlineRadio2"
+                          >
+                            Female
+                          </label>
+                        </div>
+                      </div>
+                    </MDBCol>
+                  </MDBRow>
+                  <hr />
+
+                  <MDBRow>
+                    <MDBCol sm="12" style={{ display: 'flex', alignItems: 'center' }}>
+                      <motion.button whileTap={{ scale: 1.2 }} type="submit" className="addToCart__btn">
+                        Update
+                      </motion.button>
+                    </MDBCol>
+                  </MDBRow>
+                </form>
+
               </MDBCardBody>
 
             </MDBCard>
@@ -114,7 +278,7 @@ const UserProfile = () => {
         </MDBRow>
       </MDBContainer>
     </section>
-  )
-}
+  );
+};
 
 export default UserProfile;
