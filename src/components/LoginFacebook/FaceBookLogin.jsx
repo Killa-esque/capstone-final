d6250c718bb9956157251888e2b48dcb22821cf3
MinhThu <NguyenThu212530@gmail.com>
2023-01-01 17:01:48 +0700

    final

--- src/components/LoginFacebook/FaceBookLogin.jsx
+++ src/components/LoginFacebook/FaceBookLogin.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import FacebookLogin from 'react-facebook-login'
+import FacebookLogin from 'react-facebook-login'
 import { useDispatch } from 'react-redux';
 import { getTokenFacebook } from '../../redux/reducers/userReducer';
 
@@ -10,14 +10,13 @@ export default function FaceBookLogin() {
   }
   return (
     <div>
-      {/* <FacebookLogin
+      <FacebookLogin
         appId="690988225891975"
         autoLoad={false}
         fields="name,email,picture"
         callback={responseFacebook}
         cssClass='w-100 bg-primary fw-bold py-1 border-0 text-white rounded'
-      /> */}
-      <></>
+      />
     </div>
   )
 }
